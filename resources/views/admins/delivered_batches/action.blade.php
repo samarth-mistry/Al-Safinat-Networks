10c3fdb3e755251ba279f932f90366f69edc0eb0
Samarth Mistry's DKP <samarthmistry13@outlook.com>
2021-12-30 11:25:55 +0530

    screenshot added

---
+++ resources/views/admins/delivered_batches/action.blade.php
@@ -0,0 +1,5 @@
+<span style="overflow: visible; position: relative; width: 110px;">
+	<a href="" title="Details" class="btn btn-sm btn-clean btn-icon btn-icon-md">
+		<i class="fa fa-info-circle text-blue"></i>
+	</a>
+</span>
